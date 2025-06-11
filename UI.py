9a2724388585154657579c4802ff2695927598e5
Jiayu Wang <jiayuw12@gmail.com>
2025-06-11 15:24:40 -0400

    trying things out

--- UI.py
+++ UI.py
@@ -1,5 +1,22 @@
 import streamlit as st
 import data_handling as data
 
-data.update_data
-st.write(data.all_data)
+data.update_data()
+
+st.title("Data Visualizer")
+
+# all data
+st.header("All Data")
+st.dataframe(data.all_data)
+
+# team selector
+team_numbers = list(data.data_by_team.keys())
+selected_team = st.selectbox("Select a team", team_numbers)
+
+# selected team data
+st.header(f"Data for Team {selected_team}")
+st.dataframe(data.data_by_team[selected_team])
+
+# summary for selected team
+st.header(f"Summary for Team {selected_team}")
+st.write(data.summaries_by_team[selected_team])
